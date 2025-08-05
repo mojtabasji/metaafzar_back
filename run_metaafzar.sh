72bd17d96bf683c6b88396bd020852c6de8a85ff
mojtaba <mojtabashojailine@yahoo.com>
2025-08-05 15:38:45 +0330

    fix Django user.

--- run_metaafzar.sh
+++ run_metaafzar.sh
@@ -2,5 +2,5 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 # create a superuser if it doesn't exist with password 'admin'
-echo "from django.contrib.auth import get_user_model; from django.contrib.auth.hashers import make_password; User = get_user_model(); User.objects.get_or_create(username='$DJ_USERNAME', email='$DJ_EMAIL', defaults={'password': make_password('$DJ_PASSWORD')})" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; from django.contrib.auth.hashers import make_password; User = get_user_model(); User.objects.get_or_create(username='$DJ_USERNAME', email='$DJ_EMAIL', is_superuser=True, is_staff=True, defaults={'password': make_password('$DJ_PASSWORD')})" | python manage.py shell
 python manage.py runserver 0.0.0.0:8930
